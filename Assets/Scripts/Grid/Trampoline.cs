5485b7be12960b4de1c1c421bea203ee712cad98
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2020-07-10 13:05:53 -0500

    Trampoline logic. Not tested properly.

---
+++ Assets/Scripts/Grid/Trampoline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : GridElement
+{
+    
+
+    public override void Start() {
+        this.Type = Element.Trampoline;
+        //base.Start();
+    }
+
+    public override void Update() {
+        //base.Update();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag.Equals("Player")) {
+            Vector3 direction = collision.collider.transform.position - transform.position;
+            if (direction.y > 0.95) {
+                Vector3 jumpForce = new Vector3(0, 150, 0);
+                collision.collider.GetComponent<Rigidbody>().AddForce(jumpForce);
+            }
+            
+        }
+    }
+}
