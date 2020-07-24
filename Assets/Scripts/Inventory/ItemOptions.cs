b1add75e63b6e4371948b8f901c4e266d967e6a9
Milton <A01634505@itesm.mx>
2020-07-24 15:21:54 -0500

    Edit menu for an item created with animations and a base script

---
+++ Assets/Scripts/Inventory/ItemOptions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOptions : MonoBehaviour
+{
+    Animator anim;
+    float deletionTime;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        deletionTime = anim.GetCurrentAnimatorStateInfo(0).length;
+    }
+
+    private void CloseMenu() {
+        anim.SetTrigger("Close");
+        Destroy(gameObject, deletionTime);
+    }
+
+    public void CancelButton() {
+        CloseMenu();
+    }
+
+    public void AcceptButton() {
+        CloseMenu();
+        //Accepted Code
+    }
+
+    public void RotateButton() {
+        //Rotation Code
+    }
+}
